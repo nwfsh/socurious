29c51fbf5f1e2bb64d723959d4911f6e5d368474
cxinyu14 <averycx14@gmail.com>
2026-08-26 02:10:57 -0700

    refactor: drop relationship_context tables and split overmodelling fix into migration 005 + added categories + changed columns in questions

--- migrations/005_fixing_overmodelling_and_add_categories.sql
+++ migrations/005_fixing_overmodelling_and_add_categories.sql
@@ -9,15 +9,28 @@ CREATE TABLE question_category (
     PRIMARY KEY (question_id, category_id)
 );
 
-CREATE TABLE question_relationship_context (
-    question_id     INTEGER NOT NULL REFERENCES questions(id) ON DELETE CASCADE,
-    relationship_id INTEGER NOT NULL REFERENCES relationship_context(id) ON DELETE RESTRICT,
-    PRIMARY KEY (question_id, relationship_id)
-);
 
 -- will use joins later to mix both together when needed, but in context, tags are not used as often 
 
 DROP TABLE question_tags;
+DROP TABLE relationship_context;
 
 CREATE INDEX idx_question_category ON question_category(category_id)
-CREATE INDEX idx_question_relationship_context ON question_relationship_context(relationship_id)
+
+INSERT INTO categories (name) VALUES
+    ('relationships'),
+    ('family and childhood'),
+    ('career'),
+    ('fears and insecurities'),
+    ('random everyday questions'),
+    ('hypothetical scenarios'),
+    ('sexual'),
+    ('controversial debate'),
+    ('advice')
+ON CONFLICT DO NOTHING;
+
+ALTER TABLE sources DROP COLUMN default_severity_hint;
+ALTER TABLE questions ALTER COLUMN severity TYPE NUMERIC(4,3); -- means 4 total digits, 3 decimal points
+ALTER TABLE questions RENAME COLUMN severity TO intimacy_score;
+ALTER TABLE questions ADD CONSTRAINT intimacy_score_range CHECK (intimacy_score >= -1 AND intimacy_score <= 1);
+
